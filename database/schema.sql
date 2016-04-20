1a7a5375d14ea2f51fd176e8d697341cb6deedc1
Dan Wagner <dan.wagner7@gmail.com>
2016-04-20 13:30:53 -0400

    updated database to include members table and loan table and updated keys

--- database/schema.sql
+++ database/schema.sql
@@ -7,24 +7,46 @@ BEGIN;
 CREATE TABLE tool_category (
     tool_category_id integer primary key,
     name varchar(64) not null
+    
 );
+
 
 CREATE TABLE tool (
     tool_id integer primary key,
     tool_category_id integer not null references tool_category(tool_category_id),
     name varchar(64) not null,
     description text,
-    loan_period_in_days integer not null
+    loan_period_in_days integer not null,
+    CONSTRAINT fk_tool_tool_category_id FOREIGN KEY (tool_category_id) REFERENCES tool_category(tool_category_id)
 );
 
 CREATE SEQUENCE seq_tool_id;
 
+
 CREATE TABLE tool_inventory (
     tool_inventory_id integer primary key,
-    tool_id integer not null references tool(tool_id)
+    tool_id integer not null references tool(tool_id),
+    CONSTRAINT fk_tool_inventory_tool_id FOREIGN KEY (tool_id) REFERENCES tool(tool_id)
+    
 );
 
 CREATE SEQUENCE seq_tool_inventory_id;
+
+
+CREATE TABLE loans (
+	loan_id integer not null,
+	loan_basket_id integer not null
+	loan_date date not null,
+	tool_inventory_id integer not null,
+	member_id integer not null,
+	return_date date,
+	CONSTRAINT pk_loans_loan_id PRIMARY KEY (loan_id),
+	CONSTRAINT fk_loans_member_id FOREIGN KEY (member_id) REFERENCES members(member_id),
+	CONSTRAINT fk_loans_tool_inventory_id FOREIGN KEY (tool_inventory_id) REFERENCES tool_inventory(tool_inventory_id),
+);
+
+CREATE SEQUENCE seq_loan_id;
+
 
 CREATE TABLE librarian_users (
 	librarian_username varchar(32) not null,		-- Username
@@ -32,7 +54,5 @@ CREATE TABLE librarian_users (
 	librarian_salt varchar(256) NOT NULL,			-- Password Salt
 	CONSTRAINT pk_librarian_users_librarian_username PRIMARY KEY (librarian_username)
 );
-
-
 
 COMMIT;
