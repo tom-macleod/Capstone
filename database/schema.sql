147b0102552f1efeb0e44e0c49f89b0706fb7ace
Dan Wagner <dan.wagner7@gmail.com>
2016-04-20 13:56:57 -0400

    fixed database problem

--- database/schema.sql
+++ database/schema.sql
@@ -6,11 +6,10 @@ BEGIN;
 
 CREATE TABLE tool_category (
     tool_category_id integer primary key,
-    name varchar(64) not null
-    
+    name varchar(64) not null  
 );
 
-
+CREATE SEQUENCE seq_tool_id;
 CREATE TABLE tool (
     tool_id integer primary key,
     tool_category_id integer not null references tool_category(tool_category_id),
@@ -20,9 +19,8 @@ CREATE TABLE tool (
     CONSTRAINT fk_tool_tool_category_id FOREIGN KEY (tool_category_id) REFERENCES tool_category(tool_category_id)
 );
 
-CREATE SEQUENCE seq_tool_id;
 
-
+CREATE SEQUENCE seq_tool_inventory_id;
 CREATE TABLE tool_inventory (
     tool_inventory_id integer primary key,
     tool_id integer not null references tool(tool_id),
@@ -30,22 +28,33 @@ CREATE TABLE tool_inventory (
     
 );
 
-CREATE SEQUENCE seq_tool_inventory_id;
+
+CREATE SEQUENCE seq_member_id;
+CREATE TABLE members (
+	member_id integer not null,
+	member_name varchar(128) not null,
+	member_phone varchar(10) not null,
+	member_license varchar(16) not null,
+	member_username varchar(32) not null,
+	membership_paid boolean not null,
+	member_expiration date,
+	member_unpaid_fees decimal(12,2),
+	CONSTRAINT pk_members_member_id PRIMARY KEY (member_id)
+);
 
 
+CREATE SEQUENCE seq_loan_id;
 CREATE TABLE loans (
 	loan_id integer not null,
-	loan_basket_id integer not null
+	loan_basket_id integer not null,
 	loan_date date not null,
 	tool_inventory_id integer not null,
 	member_id integer not null,
 	return_date date,
 	CONSTRAINT pk_loans_loan_id PRIMARY KEY (loan_id),
 	CONSTRAINT fk_loans_member_id FOREIGN KEY (member_id) REFERENCES members(member_id),
-	CONSTRAINT fk_loans_tool_inventory_id FOREIGN KEY (tool_inventory_id) REFERENCES tool_inventory(tool_inventory_id),
+	CONSTRAINT fk_loans_tool_inventory_id FOREIGN KEY (tool_inventory_id) REFERENCES tool_inventory(tool_inventory_id)
 );
-
-CREATE SEQUENCE seq_loan_id;
 
 
 CREATE TABLE librarian_users (
@@ -54,5 +63,6 @@ CREATE TABLE librarian_users (
 	librarian_salt varchar(256) NOT NULL,			-- Password Salt
 	CONSTRAINT pk_librarian_users_librarian_username PRIMARY KEY (librarian_username)
 );
+
 
 COMMIT;
